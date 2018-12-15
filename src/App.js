a89b51c9e0d12be80b346c3d10549eb2754dc13c
abisuresh <aesther2012@gmail.com>
2018-12-15 12:40:19 -0500

    Changed title of App on app js page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hitchhiker app
           </p>
           <a
             className="App-link"
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Get a ride easily 
           </a>
         </header>
       </div>
